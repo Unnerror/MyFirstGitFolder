b3b2dced3a3fd8cd44b9e61392b0418148846a71
Danila <danilasergienko@gmail.com>
2024-01-24 11:50:34 -0500

    added third file

---
+++ AreaOfCircleByRadius.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class AreaOfCircleByRadius  {
+	
+	public static void main(String[] args) {
+		
+		    double radius;
+		    double area;
+		    	
+		    Scanner input = new Scanner (System. in);
+		    System.out.print("Enter a double value: ");
+		    radius = input.nextDouble();
+		    
+		    
+		    area = radius * radius * 3.14159;
+		    
+		   
+		    System.out.println("The area for the circle of radius " + radius + " is " + area);
+		    
+
+		  }
+		}
+
+
+
+
